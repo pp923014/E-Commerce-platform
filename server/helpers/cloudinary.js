0c242051df6ba5e1b327c832642cf5da115d8507
pp192110 <pp923014@gmail.com>
2026-06-06 00:21:00 +0530

    setup of cludinary

--- server/helpers/cloudinary.js
+++ server/helpers/cloudinary.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const cloudinary = require("cloudinary").v2;
 const multer = require("multer");
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+  api_key:process.env.CLOUDINARY_API_KEY,
+  api_secret:process.env.CLOUDINARY_API_SECRET,
 });
 
 const storage = new multer.memoryStorage();
